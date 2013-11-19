7d5af4914d9e7aa48817abb387efb6923cf1c6f8
Nathan <njf514@mun.ca>
2013-11-19 19:31:24 -0330

    This is my tidyup of the Classes

--- Board/Direction.java
+++ Board/Direction.java
@@ -1,5 +1,8 @@
 package Board;
-
+/**
+ * This is our enumerated types for Direction for class MovementController
+ * @author nathan
+ */
 public enum Direction 
 {
     RIGHT,LEFT,UP,DOWN
--- Board/Drawable.java
+++ Board/Drawable.java
@@ -1,17 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Board;
-
 /**
  *
  * @author ooo524
  */
- import java.awt.Graphics2D;
-public interface Drawable {
-   
+import java.awt.Graphics2D;
 
-
+public interface Drawable 
+{
    public void draw(Graphics2D g2d);
+
 }
