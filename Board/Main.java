bb61f189faf567c13001fe9578bf2cdaec67a0d9
Nathan <njf514@mun.ca>
2013-11-19 19:21:20 -0330

    This is my Tidy up of the classes

--- Board/Main.java
+++ Board/Main.java
@@ -1,24 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Board;
 import Game.*;
-import Player.*;
-/**
- *
- * @author ooo524
- */
-import java.awt.*;
-import javax.swing.JMenuBar;
-
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-
-public class Main extends JFrame{
+/**
+ * This is our Main class for running the game
+ * @author nathan
+ */
+public class Main extends JFrame
+{
     
-    public Main(){
+    public Main()
+    {
         add(new Board());
         setJMenuBar(new Menu().getMenuBar());
         setSize( 870, 700 ); 
@@ -27,11 +18,11 @@ public class Main extends JFrame{
         setVisible( true );
         Movepad move = new Movepad(LocalPlayer.current_Player);
         move.setFocusable(true);
-
         add(move);
     }
         
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Main main = new Main();
         
     }
--- Game/LocalPlayer.java
+++ Game/LocalPlayer.java
@@ -1,18 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Game;
-
 import Player.*;
 /**
- *
+ * This class holds the current player of 
+ * unique game instance
  * @author cs
  */
-public class LocalPlayer {
+public class LocalPlayer 
+{
     public static PlayerProfile current_Player;
-    
-    public static void setLocalPlayer(PlayerProfile login_Player){
+    /**
+     * This method initializes the current player
+     * @param login_Player 
+     */
+    public static void setLocalPlayer(PlayerProfile login_Player)
+    {
         current_Player = login_Player;        
     }
 }
