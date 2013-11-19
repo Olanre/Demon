951a5c8796b691cc7e78385bf040eb5be7d1f436
Darwin <darwin.r.carrillo@gmail.com>
2013-11-19 19:19:54 -0330

    Cleaner code
    ActivePlayers
    Animate
    Board
    CreatePlayer

--- Board/Animate.java
+++ Board/Animate.java
@@ -1,14 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This interface is used for sprite animation
  */
+
 package Board;
 
-
-/**
- *
- * @author ooo524
- */
 public interface Animate {
     void move( double x, double y);
     void changeConfiguration();
